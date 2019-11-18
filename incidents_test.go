7e7043ec30f74c425cb02aa7e15af80addca3a2a
Ronald Lens <rflens@gmail.com>
2019-11-18 15:40:34 +0100

    add incidents.go tests refactor command line flags to struct

---
+++ incidents_test.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestIncidents_filterOutProdCategories(t *testing.T) {
+	i1 := Incident{ID: "1", ProdCategory: "foo"}
+	i2 := Incident{ID: "2", ProdCategory: "foo"}
+	i3 := Incident{ID: "3", ProdCategory: "bar"}
+
+	incidents := Incidents{i1, i2, i3}
+	prodCategories := []string{"bar"}
+
+	filtered := incidents.filterOutProdCategories(prodCategories)
+	if len(filtered) != 2 {
+		t.Errorf("Exepcted length of 2, got %d", len(filtered))
+	}
+}
+
+func TestIncidents_filterByCountry(t *testing.T) {
+	i1 := Incident{ID: "1", Country: "foo"}
+	i2 := Incident{ID: "2", Country: "bar"}
+	i3 := Incident{ID: "3", Country: "foo"}
+
+	incidents := Incidents{i1, i2, i3}
+	filtered := incidents.filterByCountry("foo")
+	if len(filtered) != 2 {
+		t.Errorf("Exepcted length of 2, got %d", len(filtered))
+	}
+}
+
+func TestIncidents_filterByPriority(t *testing.T) {
+	i1 := Incident{ID: "1", Priority: Critical}
+	i2 := Incident{ID: "2", Priority: High}
+	i3 := Incident{ID: "3", Priority: Critical}
+
+	incidents := Incidents{i1, i2, i3}
+	filtered := incidents.filterByPriority(Critical)
+	if len(filtered) != 2 {
+		t.Errorf("Exepcted length of 2, got %d", len(filtered))
+	}
+}
+
+func Test_subtractMonths(t *testing.T) {
+	type args struct {
+		month int
+		year  int
+		delta int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  int
+		want1 int
+	}{
+		{
+			name: "simple date test",
+			args: args{
+				month: 10,
+				year:  2019,
+				delta: 4,
+			},
+			want:  6,
+			want1: 2019,
+		},
+		{
+			name: "simple date test across year",
+			args: args{
+				month: 1,
+				year:  2019,
+				delta: 4,
+			},
+			want:  9,
+			want1: 2018,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := subtractMonths(tt.args.month, tt.args.year, tt.args.delta)
+			if got != tt.want {
+				t.Errorf("subtractMonths() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("subtractMonths() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func Test_getNextMonth(t *testing.T) {
+	type args struct {
+		month int
+		year  int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  int
+		want1 int
+	}{
+		{
+			name: "simple 1",
+			args: args{
+				month: 10,
+				year:  2019,
+			},
+			want:  11,
+			want1: 2019,
+		},
+		{
+			name: "simple 1 across year",
+			args: args{
+				month: 12,
+				year:  2019,
+			},
+			want:  1,
+			want1: 2020,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := getNextMonth(tt.args.month, tt.args.year)
+			if got != tt.want {
+				t.Errorf("getNextMonth() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("getNextMonth() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func Test_getPreviousMonth(t *testing.T) {
+	type args struct {
+		month int
+		year  int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  int
+		want1 int
+	}{
+		{
+			name: "simple 1",
+			args: args{
+				month: 10,
+				year:  2019,
+			},
+			want:  9,
+			want1: 2019,
+		},
+		{
+			name: "simple 1 across year",
+			args: args{
+				month: 1,
+				year:  2019,
+			},
+			want:  12,
+			want1: 2018,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := getPreviousMonth(tt.args.month, tt.args.year)
+			if got != tt.want {
+				t.Errorf("getPreviousMonth() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("getPreviousMonth() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
--- main.go
+++ main.go
@@ -9,29 +9,31 @@ import (
 )
 
 // command line arguments
-var configFilename string
-var inputFilename string
-var referenceFilename string
-var outputFilename string
-var country string
-var month int
-var year int
-var now bool
-var verbose bool
+var flagVars struct {
+	configFilename    string
+	inputFilename     string
+	referenceFilename string
+	outputFilename    string
+	country           string
+	month             int
+	year              int
+	now               bool
+	verbose           bool
+}
 
 func init() {
 	// set up all command line flags
-	flag.StringVar(&configFilename, "cfg", "goreport.yaml", "Configuration filename")
-	flag.StringVar(&inputFilename, "input", "allincidents.csv", "Tab delimited incident input filename")
-	flag.StringVar(&referenceFilename, "reference", "", "Excel file to use as input reference")
-	flag.StringVar(&outputFilename, "output", "", "Output filename to use for xlsx file")
-	flag.StringVar(&country, "country", "", "Country to report on")
+	flag.StringVar(&flagVars.configFilename, "cfg", "goreport.yaml", "Configuration filename")
+	flag.StringVar(&flagVars.inputFilename, "input", "allincidents.csv", "Tab delimited incident input filename")
+	flag.StringVar(&flagVars.referenceFilename, "reference", "", "Excel file to use as input reference")
+	flag.StringVar(&flagVars.outputFilename, "output", "", "Output filename to use for xlsx file")
+	flag.StringVar(&flagVars.country, "country", "", "Country to report on")
 
-	flag.IntVar(&month, "month", -1, "Month to report on (1..12)")
-	flag.IntVar(&year, "year", -1, "Year to report on")
-	flag.BoolVar(&now, "now", false, "Use current month instead of last month")
+	flag.IntVar(&flagVars.month, "month", -1, "Month to report on (1..12)")
+	flag.IntVar(&flagVars.year, "year", -1, "Year to report on")
+	flag.BoolVar(&flagVars.now, "now", false, "Use current month instead of last month")
 
-	flag.BoolVar(&verbose, "v", false, "Increased verbosity")
+	flag.BoolVar(&flagVars.verbose, "v", false, "Increased verbosity")
 
 }
 
@@ -41,82 +43,82 @@ func main() {
 	flag.Parse()
 
 	// read configuration file
-	config, err := readConfig(configFilename)
+	config, err := readConfig(flagVars.configFilename)
 	if err != nil {
 		log.Fatalf("Error reading config file: %v", err)
 	}
 
 	// get country from config file
 	// or defined via command lne args
-	if country == "" {
-		country = config.DefaultCountry
+	if flagVars.country == "" {
+		flagVars.country = config.DefaultCountry
 	}
 
 	// if no month or year was supplied
 	// use last month unless now was supplied as an option
-	if month == -1 || year == -1 {
-		month = int(time.Now().Month())
-		year = time.Now().Year()
-		if !now {
-			month, year = getPreviousMonth(month, year)
+	if flagVars.month == -1 || flagVars.year == -1 {
+		flagVars.month = int(time.Now().Month())
+		flagVars.year = time.Now().Year()
+		if !flagVars.now {
+			flagVars.month, flagVars.year = getPreviousMonth(flagVars.month, flagVars.year)
 		}
 	}
 
 	// load the incidents
-	incidents, err := importIncidents(inputFilename)
+	incidents, err := importIncidents(flagVars.inputFilename)
 	if err != nil {
-		log.Fatalf("Error importing %s: %v", inputFilename, err)
+		log.Fatalf("Error importing %s: %v", flagVars.inputFilename, err)
 	}
-	if verbose {
-		log.Printf("Loaded a total of %d incidents from %s\n", len(incidents), inputFilename)
+	if flagVars.verbose {
+		log.Printf("Loaded a total of %d incidents from %s\n", len(incidents), flagVars.inputFilename)
 	}
 
 	// work through commands
 	if hasCommand("list") {
 		if hasNoun("countries") {
-			if verbose {
+			if flagVars.verbose {
 				log.Printf("Listing all countries")
 			}
 			listCountries(incidents)
 		}
 		if hasNoun("prodcategories") {
-			if country != "" {
-				if verbose {
-					log.Printf("Filtering by country %s", country)
+			if flagVars.country != "" {
+				if flagVars.verbose {
+					log.Printf("Filtering by country %s", flagVars.country)
 				}
-				incidents = incidents.filterByCountry(country)
+				incidents = incidents.filterByCountry(flagVars.country)
 			}
 			listProductCategories(incidents)
 		}
 	} else if hasCommand("report") {
 		// reduce incidents
-		countryConfig := getCountryFromConfig(config, country)
-		incidents = incidents.filterByCountry(country)
+		countryConfig := getCountryFromConfig(config, flagVars.country)
+		incidents = incidents.filterByCountry(flagVars.country)
 		incidents = incidents.filterOutProdCategories(countryConfig.FilterOutCategories)
 
 		// if we are to use a reference xlsx, process it
 		// if the name equals to 'same' use the same name as the output
 		// if the name equals to 'previous' or 'prev' use the xlsx from last month
-		if referenceFilename != "" {
-			if referenceFilename == "same" {
-				referenceFilename = getFilename(country, month, year)
-			} else if referenceFilename == "previous" || referenceFilename == "prev" {
-				prevMonth, prevYear := getPreviousMonth(month, year)
-				referenceFilename = getFilename(country, prevMonth, prevYear)
+		if flagVars.referenceFilename != "" {
+			if flagVars.referenceFilename == "same" {
+				flagVars.referenceFilename = getFilename(flagVars.country, flagVars.month, flagVars.year)
+			} else if flagVars.referenceFilename == "previous" || flagVars.referenceFilename == "prev" {
+				prevMonth, prevYear := getPreviousMonth(flagVars.month, flagVars.year)
+				flagVars.referenceFilename = getFilename(flagVars.country, prevMonth, prevYear)
 			}
-			incidents = ProcessReferenceFile(incidents, referenceFilename)
+			incidents = ProcessReferenceFile(incidents, flagVars.referenceFilename)
 		}
 
 		slaSet := ParseSLAConfig(countryConfig.SLAs)
 		incidents = checkIncidentsAgainstSLA(incidents, slaSet)
-		runReport(&incidents, country, month, year, countryConfig.SplitArea, outputFilename, countryConfig.MinimumIncidents, verbose)
+		runReport(&incidents, flagVars.country, flagVars.month, flagVars.year, countryConfig.SplitArea, flagVars.outputFilename, countryConfig.MinimumIncidents, flagVars.verbose)
 	} else if hasCommand("gui") {
 		RunGui(incidents)
 	} else {
 		log.Fatalf("No command specified")
 	}
 
-	if verbose {
+	if flagVars.verbose {
 		log.Printf("Total running time: %s\n", time.Since(start))
 	}
 }
